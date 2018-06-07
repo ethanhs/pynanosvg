b172ce8e20f25358be305c5b0a231322f81e7c64
Ethan Smith <ethan@ethanhs.me>
2018-06-07 16:26:04 -0700

    0.2

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup, Extension
 from Cython.Build import cythonize
 import os
+from os import path
 import sys
 
 
@@ -8,12 +9,16 @@ if sys.version_info < (3, 5) and os.name == 'nt':
     print("Nanosvg cannot be built on Python 3.4 or below on Windows.")
     sys.exit(1)
 
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 ext = [Extension('svg', sources=['svg/__init__.pyx'],
                  include_dirs=['nanosvg/src/', 'pynanosvg/'])]
 
 setup(
     name="pynanosvg",
-    version="0.1.1",
+    version="0.2.0",
     description="Wrapper around nanosvg",
     author="Ethan Smith",
     author_email="ethan@ethanhs.me",
@@ -21,5 +26,7 @@ setup(
     license="MIT",
     ext_modules=cythonize(ext),
     packages=['svg-stubs'],
-    package_data={'svg-stubs': ['svg.pyi']}
+    package_data={'svg-stubs': ['svg.pyi']},
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 )
