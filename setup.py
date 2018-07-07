49b728e3d51ccfb8f438e07176610fe03b13a988
Ethan Smith <ethan@ethanhs.me>
2018-07-07 11:30:19 -0700

    Use cibuildwheel to build wheels

--- setup.py
+++ setup.py
@@ -4,17 +4,12 @@ import os
 from os import path
 import sys
 
-
-if sys.version_info < (3, 5) and os.name == 'nt':
-    print("Nanosvg cannot be built on Python 3.4 or below on Windows.")
-    sys.exit(1)
-
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, 'README.md')) as f:
     long_description = f.read()
 
 ext = [Extension('svg', sources=['svg/__init__.pyx'],
-                 include_dirs=['nanosvg/src/', 'pynanosvg/'])]
+                 include_dirs=['./nanosvg/src/', './pynanosvg/'])]
 
 setup(
     name="pynanosvg",
